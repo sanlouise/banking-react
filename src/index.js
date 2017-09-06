4048363c08c43808338d8f52ceb9898c0c217102
sanlouise <sandra.adamshallie@gmail.com>
2017-09-06 09:26:25 -0400

    Import components and containers, set up BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import appReducer from './containers/App/appReducer';
+
+//router imports
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//Import styles
+import './index.css';
+
+//Import components
+import App from './containers/App';
+import BaseLayout from './components/BaseLayout';
+import NavBar from './components/NavBar';
 
 const rootReducer = combineReducers({
   app: appReducer,
@@ -13,9 +23,19 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-ReactDOM.render((
+//wrap provider around router
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <BaseLayout>
+        <NavBar>
+          <Switch>
+            <Route exact path="/" component={App} />
+          </Switch>
+        </NavBar>
+      </BaseLayout>
+    </BrowserRouter>
   </Provider>
-), document.getElementById('root'));
-registerServiceWorker();
+
+
+  , document.getElementById('root'));
