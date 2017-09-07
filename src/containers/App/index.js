05213f19d952b8684c07247e7b2d132c11372cb8
sanlouise <sandra.adamshallie@gmail.com>
2017-09-07 09:19:44 -0400

    Add individual reducers

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React, {this.props.name}</h2>
+      <div className="main">
+        <div className= "jumbotron">
+          <h1 className= "display-3">Banking Appt</h1>
+          <p>Your world wide banking leader.</p>
+          <p>Accurate account results.</p>
+          <p className= "lead">
+            <Link className= "btn btn-primary btn-lg" to="/users" role="button">Check Users</Link>
+          </p>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,26 +4,39 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
-import appReducer from './containers/App/appReducer';
 
-//router imports
+//Import router
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //Import styles
 import './index.css';
+
+//Import Reducers
+import transactionReducer from './containers/Transaction/reducer.js';
+import appReducer from './containers/App/reducer.js';
+import userListReducer from './containers/UserList/reducer.js';
+import accountDetailReducer from './containers/AccountDetail/reducer.js';
 
 //Import components
 import App from './containers/App';
 import BaseLayout from './components/BaseLayout';
 import NavBar from './components/NavBar';
 
+//Import containers
+import AccountDetail from './containers/AccountDetail';
+import Transaction from './containers/Transaction';
+import UserList from './containers/UserList';
+
 const rootReducer = combineReducers({
   app: appReducer,
+  accountDetail: accountDetailReducer,
+  userList: userListReducer,
+  transaction: transactionReducer,
 })
 
 const store = createStore(rootReducer);
 
-//wrap provider around router
+//Wrap Provider around Router
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
