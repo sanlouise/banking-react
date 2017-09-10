b983130fcd0e7367187fde0cc73d32a66081457a
sanlouise <sandra.adamshallie@gmail.com>
2017-09-10 17:42:53 -0400

    Remove unnecessary reducers from index.js

--- src/containers/Users/reducer.js
+++ src/containers/Users/reducer.js
@@ -1,43 +1,10 @@
-import { combineReducers } from 'redux';
-import {
-  ACCOUNT_SELECTED,
-  USER_SELECTED,
-  WITHDRAW_FUNDS,
-} from './constants';
+import users from './usersData';
 
-import getInitialUsers from './initialUsers';
-
-
-const initialState = {
-  users: getInitialUsers(),
-  selectedUser: {},
-  selectedAccount: {},
-};
-
+const initialState = users;
 
 const usersReducer = function(state = initialState, action) {
   switch (action.type) {
-    case USER_SELECTED: {
-      return {
-        ...state,
-        selectedUser: action.selectedUser,
-      };
-    }
-    case ACCOUNT_SELECTED: {
-      return {
-        ...state,
-        selectedAccount: {},
-      };
-    }
-    case WITHDRAW_FUNDS:{
-      return {
-        ...state,
-        selectedAccount: {
-          ...state.selectedAccount,
-          balance: state.selectedAccount.balance - action.amount,
-        },
-      };
-    }
+
     default: {
       return state;
     }
