943fc41c5edce073c2209351a116312df9237e61
sanlouise <sandra.adamshallie@gmail.com>
2017-09-10 17:45:03 -0400

    Remove constants

--- src/containers/Users/constants.js
+++
@@ -1,4 +0,0 @@
-// Delcare constants for actions.
-export const USER_SELECTED = 'Users/USER_SELECTED';
-export const ACCOUNT_SELECTED = 'Users/ACCOUNT_SELECTED';
-export const WITHDRAW_FUNDS = 'Users/WITHDRAW_FUNDS';
