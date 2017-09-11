670288b07117abd6ba8d099ba1eb586b89183e02
sanlouise <sandra.adamshallie@gmail.com>
2017-09-11 12:40:11 -0400

    Add Accordion to UserDetail container

--- src/containers/UserDetail/index.js
+++ src/containers/UserDetail/index.js
@@ -2,26 +2,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import map from 'lodash/map';
+import PropTypes from 'prop-types';
+// Externals
+import Accordion from '../../components/Accordion';
 
 
-const UserDetail = ({ user }) => (
-  <div className="user">
-    <ul className="info" key={user._id}>
-      <li>Name: {user.name}</li>
-      <li>Email: {user.email}</li>
-      <li>Phone: {user.phone}</li>
-      <li>Address: {user.address}</li>
-    </ul>
-    <ul className="accounts">
-      {map(user.accounts, (account) => (
-        <ul className="account" key={account.id}>
-          <li>Account Type: {account.accountType}</li>
-          <li>Balance: USD{account.balance}</li>
-        </ul>
-      ))}
-    </ul>
-  </div>
-);
+const UserDetail = ({ user }) => {
+  const collapsibles = map(user.accounts, (account) => ({
+    header: `Account Type: ${account.accountType}, #${account.id}`,
+    body: `Balance: ${account.balance}`,
+  }));
+
+  return (
+    <div className="user center">
+      <ul className="info" key={user._id}>
+        <li>Name: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>Phone: {user.phone}</li>
+        <li>Address: {user.address}</li>
+      </ul>
+      <Accordion collapsibles={collapsibles} />
+    </div>
+  );
+}
 
 function mapStateToProps(state, props) {
   const userId = state.users.findIndex(user => user._id === props.match.params.userId);
